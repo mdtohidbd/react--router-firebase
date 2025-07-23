3697e978bb782dc17ecf4f49b2a446941c8e76fd
mdtohidvai <mdtohid242024@gmail.com>
2025-07-24 00:20:28 +0600

    this is Login and navbar Update

---
+++ src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Register extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Register</h3>
+      </div>
+    )
+  }
+}
+
+export default Register
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYFobThKWH6wpvoOeafFlduNbqArH9WE0",
+  authDomain: "auth-moha-9148a.firebaseapp.com",
+  projectId: "auth-moha-9148a",
+  storageBucket: "auth-moha-9148a.firebasestorage.app",
+  messagingSenderId: "31591299619",
+  appId: "1:31591299619:web:32f60c82a2393ae5928f96"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
